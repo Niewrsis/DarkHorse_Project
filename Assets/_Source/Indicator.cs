26df6bcc0efdba7d67005b14237d9e7558619463
AFK-Shark <graffin.may@yandex.ru>
2025-04-06 16:18:17 +0300

    fix: beer controll

--- Assets/_Source/Indicator.cs
+++ Assets/_Source/Indicator.cs
@@ -3,26 +3,24 @@ using UnityEngine.UI;
 
 public class Indicator : MonoBehaviour
 {
-    public SpriteRenderer indicatorRenderer; // Компонент SpriteRenderer для индикатора
-    public Sprite indicator1; // Спрайт индикатора 1
-    public Sprite indicator2; // Спрайт индикатора 2
-    public Image image1; // Первое изображение
-    public Image image2; // Второе изображение
+    public SpriteRenderer indicatorRenderer;
+    public Sprite indicator1;
+    public Sprite indicator2;
+    public Image image1;
+    public Image image2;
 
     private void Update()
     {
-        // Получаем fillAmount обоих изображений
         float fillAmount1 = image1.fillAmount;
         float fillAmount2 = image2.fillAmount;
 
-        // Проверяем условие для отображения индикатора 1 или 2
         if (fillAmount1 >= 0.78560f && fillAmount1 <= 0.88782f)
         {
-            indicatorRenderer.sprite = indicator1; // Устанавливаем индикатор 1
+            indicatorRenderer.sprite = indicator1;
         }
         else
         {
-            indicatorRenderer.sprite = indicator2; // Устанавливаем индикатор 2
+            indicatorRenderer.sprite = indicator2;
         }
     }
 }
